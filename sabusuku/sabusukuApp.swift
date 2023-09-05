56f4705e5586871e204745a5547bbda0da253c75
karirin <karirin419@gmail.com>
2023-09-05 23:32:14 +0900

    Initial Commit

---
+++ sabusuku/sabusukuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sabusukuApp.swift
+//  sabusuku
+//
+//  Created by hashimo ryoya on 2023/09/05.
+//
+
+import SwiftUI
+
+@main
+struct sabusukuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
